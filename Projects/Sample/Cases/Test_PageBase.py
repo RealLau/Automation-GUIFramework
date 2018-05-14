4db6dae50249987dfcd61c80eb649a8ccbd5d2eb
RealLau <921363139@qq.com>
2018-05-14 18:10:25 +0800

    Sample web test cases.

--- Projects/Sample/Cases/Test_PageBase.py
+++ Projects/Sample/Cases/Test_PageBase.py
@@ -1,6 +1,7 @@
 # _*_ coding=utf-8 _*_
 from Projects.Sample.Pages.PageBase import PageBase
 import unittest
+from BaseDriver.Driver import AutoDriver
 
 
 class Test_PageBase(unittest.TestCase):
@@ -12,12 +13,13 @@ class Test_PageBase(unittest.TestCase):
     def setUp(self):
         self.PageBase.login(self.email, self.pwd)
 
+    @AutoDriver.restart_browser
     def tearDown(self):
-        self.PageBase.logout()
+        pass
 
     def test_verify_nickname(self):
-        expected_nickname = self.PageBase.get_config_user_info(1)["nickname"]
+        expected_nickname = self.PageBase.get_config_user_info(2)["nickname"]
         actual_nickname = self.PageBase.get_current_user_nickname()
-        assert expected_nickname == actual_nickname, "注册新设备页面title校验失败, 期望: %s, 实际: %s" % (expected_nickname,
-                                                                                           actual_nickname)
+        assert expected_nickname == actual_nickname, "登录后用户昵称校验失败, 期望: %s, 实际: %s" % (expected_nickname, actual_nickname)
+
 
